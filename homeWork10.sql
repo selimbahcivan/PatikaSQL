e5d8808c297153d251b44d1d0731a42385528f4f
YavuzSelim <y.selimbahcivan@gmail.com>
2022-02-26 23:14:03 +0300

    LEFT, RIGHT, FULL JOIN komutları kullanıldı.

---
+++ homeWork10.sql
@@ -0,0 +1,11 @@
+SELECT city.city, country.country 
+FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+SELECT customer.first_name, customer.last_name, payment.payment_id 
+FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+SELECT customer.first_name, customer.last_name, rental.rental_id 
+FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
